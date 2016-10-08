b33156413482fd812d954b8f0f7a5ba9b65fe99d
guotianyang <young.guotian@gmail.com>
2016-10-08 11:50:38 -0400

    edit 2.1
    edit 2.1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("test for github");
-		System.out.println("edit 1");
-		System.out.println("edit 2");
+		System.out.println("edit 1.");
+		System.out.println("edit 2.");
 
 	}
 
